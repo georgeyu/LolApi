c75b81c1294dca1b5e1d0fe6c6f83b0290060490
George Yu <georgelyu@gmail.com>
2017-07-22 21:37:03 -0500

    Add server

---
+++ LolApi/Server.cs
@@ -0,0 +1,75 @@
+﻿using log4net;
+using System.Net;
+using System.Reflection;
+using System.Threading;
+
+namespace LolApi
+{
+    internal class Server
+    {
+        private const int BadRequest = 400;
+        private const int Unauthorized = 401;
+        private const int Forbidden = 403;
+        private const int NotFound = 404;
+        private const int RateLimit = 429;
+        private const int ServerError = 500;
+        private const int ServiceUnavailable = 503;
+        private const int SecsToMillisecs = 1000;
+        private const int MinutesToSecs = 60;
+        private static readonly MethodBase constructor = MethodBase.GetCurrentMethod();
+        private static readonly ILog log = LogManager.GetLogger(constructor.DeclaringType);
+        private readonly WebClient client;
+
+        public Server(WebClient client)
+        {
+            this.client = client;
+        }
+
+        public string Respond(string url)
+        {
+            var responded = false;
+            string response = "";
+            while (!responded)
+            {
+                responded = TryDownload(url, out response);
+            }
+            return response;
+        }
+
+        private bool TryDownload(string url, out string response)
+        {
+            try
+            {
+                response = client.DownloadString(url);
+                return true;
+            }
+            catch (WebException e)
+            {
+                var webResponse = (HttpWebResponse)e.Response;
+                var statusCode = (int)webResponse.StatusCode;
+                var exception = e.ToString();
+                switch (statusCode)
+                {
+                    case BadRequest:
+                    case Unauthorized:
+                    case Forbidden:
+                    case NotFound:
+                        log.FatalFormat("{0} {1}", exception, url);
+                        throw e;
+                    case RateLimit:
+                    case ServerError:
+                        Thread.Sleep(SecsToMillisecs);
+                        break;
+                    case ServiceUnavailable:
+                        Thread.Sleep(MinutesToSecs * SecsToMillisecs);
+                        break;
+                    default:
+                        log.FatalFormat("Status code not handled. {0}. {1} {2}", statusCode, exception, url);
+                        throw e;
+                }
+                response = "";
+                return false;
+            }
+        }
+    }
+}
---
+++ LolApiTest/ServerTest.cs
@@ -0,0 +1,46 @@
+﻿using log4net;
+using log4net.Config;
+using LolApi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Reflection;
+
+namespace LolApiTest
+{
+    [TestClass]
+    public class ServerTest
+    {
+        private const string SummonerIDUrl = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/concise?api_key=3d7ef075-ffb6-4757-a92d-f40df1a400ce";
+        private const string ForbiddenUrl = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/concise?api_key=3d7ef075-ffb6-4757-a92d-f40df1a400cf";
+        private const string SummonerIDResponse = "{\"id\":21704191,\"accountId\":35174352,\"name\":\"Concise\",\"profileIconId\":2085,\"revisionDate\":1500771565000,\"summonerLevel\":30}";
+        private static readonly MethodBase constructor = MethodBase.GetCurrentMethod();
+        private static readonly ILog log = LogManager.GetLogger(constructor.DeclaringType);
+
+        public ServerTest()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        [TestMethod]
+        public void Respond_SummonerID_Response()
+        {
+            using (var client = new WebClient())
+            {
+                var server = new Server(client);
+                var response = server.Respond(SummonerIDUrl);
+                Assert.AreEqual(response, SummonerIDResponse);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(WebException))]
+        public void Respond_Forbidden_WebException()
+        {
+            using (var client = new WebClient())
+            {
+                var server = new Server(client);
+                var response = server.Respond(ForbiddenUrl);
+            }
+        }
+    }
+}
