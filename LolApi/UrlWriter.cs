ffadbeb9a480d5b30f1de1c0312e2d2cbbed05fa
George Yu <georgelyu@gmail.com>
2017-07-23 22:47:23 -0500

    Add URL writer

---
+++ LolApi/UrlWriter.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+
+namespace LolApi
+{
+    internal class UrlWriter
+    {
+        private const string RankedSoloRift = "4";
+        private const string RankedTeamRift = "42";
+        private const string RankedTeamBuilderTeamRift = "410";
+        private const string RankedTeamBuilderSoloRift = "420";
+        private const string RankedFlexRift = "440";
+        public readonly string rankedRiftQueues;
+        public readonly string rankedRiftSoloQueues;
+        private readonly string urlHead;
+        private readonly string keyField;
+
+        public UrlWriter(string region, string key)
+        {
+            rankedRiftQueues = GetQueueField(
+                RankedSoloRift,
+                RankedTeamRift,
+                RankedTeamBuilderTeamRift,
+                RankedTeamBuilderSoloRift,
+                RankedFlexRift);
+            rankedRiftSoloQueues = GetQueueField(RankedSoloRift, RankedTeamBuilderSoloRift);
+            urlHead = string.Format("https://{0}.api.riotgames.com/lol", region);
+            keyField = "api_key=" + key;
+        }
+
+        public string WriteSummoner(string summonerName)
+        {
+            return string.Format("{0}/summoner/v3/summoners/by-name/{1}?{2}", urlHead, summonerName, keyField);
+        }
+
+        public string WriteMatchByAccount(string accountID, string queues)
+        {
+            return string.Format(
+                "{0}/match/v3/matchlists/by-account/{1}?{2}&{3}",
+                urlHead,
+                accountID,
+                queues,
+                keyField);
+        }
+
+        public string WriteMatchByMatch(string matchID)
+        {
+            return string.Format("{0}/match/v3/matches/{1}?{2}", urlHead, matchID, keyField);
+        }
+
+        public string WriteStaticChamps()
+        {
+            return string.Format("{0}/static-data/v3/champions?locale=en_US&dataById=false&{1}", urlHead, keyField);
+        }
+
+        private string GetQueueField(params string[] queues)
+        {
+            var fields = queues.Select(x => "queue=" + x);
+            return string.Join("&", fields);
+        }
+    }
+}
---
+++ LolApiTest/UrlWriterTest.cs
@@ -0,0 +1,43 @@
+﻿using LolApi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LolApiTest
+{
+    [TestClass]
+    public class UrlWriterTest
+    {
+        private const string SummonerUrl = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/concise?api_key=123";
+        private const string MatchByAccountUrl = "https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/456?queue=4&queue=420&api_key=123";
+        private const string MatchByMatchUrl = "https://na1.api.riotgames.com/lol/match/v3/matches/456?api_key=123";
+        private const string StaticChampsUrl = "https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=123";
+        private readonly UrlWriter urlWriter = new UrlWriter("na1", "123");
+
+        [TestMethod]
+        public void WriteSummoner_Concise_Write()
+        {
+            var url = urlWriter.WriteSummoner("concise");
+            Assert.AreEqual(url, SummonerUrl);
+        }
+
+        [TestMethod]
+        public void WriteMatchByAccount_ConciseRankedRiftSolo_Write()
+        {
+            var url = urlWriter.WriteMatchByAccount("456", urlWriter.rankedRiftSoloQueues);
+            Assert.AreEqual(url, MatchByAccountUrl);
+        }
+
+        [TestMethod]
+        public void WriteMatchByMatch_MatchID_Write()
+        {
+            var url = urlWriter.WriteMatchByMatch("456");
+            Assert.AreEqual(url, MatchByMatchUrl);
+        }
+
+        [TestMethod]
+        public void WriteStaticChamps_None_Write()
+        {
+            var url = urlWriter.WriteStaticChamps();
+            Assert.AreEqual(url, StaticChampsUrl);
+        }
+    }
+}
